9f66d2f161a8d948e154b20abfb81a90f0e39d0b
che2rish <whdkfka101@naver.com>
2022-02-22 20:36:40 +0900

    0222

---
+++ JavaStudy/Chapter3/ch03/VIPCustomer.java
@@ -0,0 +1,31 @@
+package ch03;
+
+public class VIPCustomer extends Customer {
+
+    double salesRatio;
+    private String agentID;
+
+//    public VIPCustomer(){
+//        super(0,"no-name"); // 상위 클래스의 생성자를 명시적으로 호출
+//
+//        bonusRatio = 0.05;
+//        salesRatio = 0.1;
+//        customerGrade = "VIP";
+//
+//        System.out.println("VIPCustomer() show");
+//    }
+
+    public VIPCustomer(int customerID, String customerName){
+        super(customerID,customerName);
+
+        customerGrade = "VIP";
+        bonusRatio = 0.05;
+        salesRatio = 0.1;
+
+        System.out.println("VIPCustomer() show");
+    }
+
+    public String getAgentID(){
+        return agentID;
+    }
+}
---
+++ JavaStudy/Chapter3/ch04/VIPCustomer.java
@@ -0,0 +1,37 @@
+package ch04;
+
+public class VIPCustomer extends Customer {
+
+    double salesRatio;
+    private String agentID;
+
+//    public VIPCustomer(){
+//        super(0,"no-name"); // 상위 클래스의 생성자를 명시적으로 호출
+//
+//        bonusRatio = 0.05;
+//        salesRatio = 0.1;
+//        customerGrade = "VIP";
+//
+//        System.out.println("VIPCustomer() show");
+//    }
+
+    public VIPCustomer(int customerID, String customerName){
+        super(customerID,customerName);
+
+        customerGrade = "VIP";
+        bonusRatio = 0.05;
+        salesRatio = 0.1;
+
+    }
+
+    @Override
+    public int calcPrice(int price) {
+        bonusPoint += price * bonusRatio;
+        price -= (int)(price * salesRatio);
+        return super.calcPrice(price);
+    }
+
+    public String getAgentID(){
+        return agentID;
+    }
+}
