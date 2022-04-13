5d98d3f60d615df2e47a6096351842b6758a856c
che2rish <whdkfka101@naver.com>
2022-04-13 17:12:59 +0900

    07. 자료형

---
+++ there-java-programing/ch01/VarEx01.java
@@ -0,0 +1,20 @@
+package ch01;
+
+// 패키지 = 자바 파일이 모여있는 폴더
+public class VarEx01 {
+    public static void main(String[] args) {
+
+        // 참 or 거짓
+        boolean b1 = true; // 박스에 1이 저장
+        boolean b2 = false; // 박스에 0이 저장
+
+        // 정수형
+        int n1 = 1000;
+
+        // 실수형
+        double d1 = 1000.1;
+
+        // 문자형
+        char c1 = '가';
+    }
+}
