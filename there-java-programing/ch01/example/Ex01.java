b80f08027738302e2178bb7d07830452d81cdb92
che2rish <whdkfka101@naver.com>
2022-04-16 14:05:16 +0900

    연습문제

---
+++ there-java-programing/ch01/example/Ex01.java
@@ -0,0 +1,15 @@
+package ch01.example;
+
+// 붕어빵을 표현하기 위한 커스텀 자료형 (heap)을 만드시오
+// 클래스명 : 붕어빵
+// 필드 : 붕어빵 가격 1000, 맛 달콤함, 색깔 노란색
+class 붕어빵{
+    int price = 1000;
+    String taste = "달콤함";
+    String color = "노란색";
+}
+public class Ex01 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ there-java-programing/ch01/example/Ex02.java
@@ -0,0 +1,13 @@
+package ch01.example;
+
+// 메모장 코드를 작성하세요!
+// 파일명 : HelloWorld.java
+// 폴더 : ch01
+// HelloWorld 출력하는 프로그램
+// cmd 컴파일 -> 실행
+// javac 클래스명 - 컴파일 => java 클래스명 - 실행
+public class Ex02 {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
